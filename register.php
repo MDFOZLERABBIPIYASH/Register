9cc1cad80f2659665c80d665dae415d6f21cec0e
MD Fozle Rabbi Piyash <54602251+MDFOZLERABBIPIYASH@users.noreply.github.com>
2021-12-28 16:10:41 +0600

    Make Register Interface and connect database with PHP

---
+++ register.php
@@ -0,0 +1,40 @@
+<?php
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'test01';
+
+    $conn = mysqli_connect($DATABASE_HOST,$DATABASE_USER,$DATABASE_PASS,$DATABASE_NAME);
+
+    if(mysqli_connect_error()){
+        exit('Error connecting to the database' . mysqli_connect_error());
+    }
+    if(!isset($_POST['username'], $_POST['password'], $_POST['email'])){
+        exit('Empty Field(s)');
+    }
+    if(empty($_POST['username'] || $_POST['password'] || $_POST['email'])){
+        exit('Values Empty');
+    }
+    if($stmt = $conn->prepare('SELECT id, password FROM register WHERE username = ?')){
+        $stmt->bind_param('s',$_POST['username']);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if($stmt->num_rows>0){
+            echo "Username Already Exists. Try Again";
+        }else{
+            if($stmt=$conn->prepare('INSERT INTO register (username,password,email) VALUES (?,?,?)')){
+                $password=password_hash($_POST['password'],PASSWORD_DEFAULT);
+                $stmt->bind_param('sss', $_POST['username'],$password, $_POST['email']);
+                $stmt->execute();
+                echo "Succesfully Registered";
+            }else{
+                echo "Error Occurred";
+            }
+        }
+        $stmt->close();
+    }else{
+        echo "Error Occorred";
+    }
+    $conn->close();
+?>
